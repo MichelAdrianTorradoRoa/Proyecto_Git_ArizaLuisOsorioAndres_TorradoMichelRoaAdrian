678c718add672075aabda86c41c2836a3a60d3b6
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2024-07-12 17:13:46 -0500

    style(json): :fire: JSON
    Se elimino jsons
    
    Se hizo eliminacion de unos json de más

---
+++ datos.js
@@ -0,0 +1,18 @@
+
+{
+    "consulta": [
+        {
+            "nombre": "Bucaramanga ",
+            "codigo postal": "1095948960",
+            "Por pais": "Colombia",
+            "Leer por Ciudad": "",
+            "nombre": "",
+            "": ,
+            "": ,
+            "": ""
+        }
+    ]
+}
+
+
+
